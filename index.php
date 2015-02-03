2326f6e9b61dca458830f5c676d908b06e52d22d
Luca <ldanzelmo@gmail.com>
2015-02-03 22:50:52 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+// Import core libraries.
+require_once('core/Registry.php');
+require('core/Configloader.php');
+require_once('core/Database.php');
+
+try {
+
+    // Get an instance of the Registry
+    $registry = \acd\Registry::getInstance();
+
+    // Load configuration into the registry
+    $registry->set('config', new \acd\Configloader('include/config.ini'));
+
+    // Connect to database
+    \acd\Database::connect(\acd\Registry::getInstance());
+
+} catch (\Exception $e) {
+    echo $e->getMessage();
+}
+
+\acd\Database::disconnect();
