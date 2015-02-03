e81a09916b0d7a589841cab0c4fa7bc8e0c6fea7
acidvertigo <ldanzelmo@gmail.com>
2015-02-03 22:59:19 +0100

    Initial commit

---
+++ core/Database.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace acd;
+
+/**
+ * Description of database
+ *
+ * @author Acidvertigo
+ */
+class Database 
+{
+
+    private static $conn = null;
+    private $db = array();
+
+    private function __construct() {}
+    
+    private function __clone() {}
+
+    public static function connect(Registry $registry)
+    {
+        // One connection through whole application
+        if (null == self::$conn) {
+            try {
+                // Then load the database connection parameters, ...
+                $db = $registry->get('config')['database'];
+                self::$conn = new \PDO("mysql:host=".$db['HOST'].";dbname=".$db['NAME'], $db['USERNAME'], $db['PASSWORD']);
+            } catch (\PDOException $e) {
+                echo $e->getMessage();
+            }
+        }
+        return self::$conn;
+    }
+
+    public static function disconnect()
+    {
+        self::$conn = null;
+    }
+
+}
---
+++ core/Registry.php
@@ -0,0 +1,98 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace acd;
+
+/**
+ * Registry class
+ */
+class Registry implements \ArrayAccess
+{
+
+    /** @var array Registry configuration array **/
+    private $registry = array();
+
+    /** @var void Class instance **/
+    private static $instance = null;
+
+    /**
+     * Gets an istance of the Registry class
+     * @return object
+     */
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new Registry();
+        }
+
+        return self::$instance;
+    }
+
+    private function __construct() {}
+
+    private function __clone() {}
+
+    /**
+     * Adds element to registry array
+     * @param string $key
+     * @param string $value
+     * @throws Exception When there is a duplicate $key
+     */
+    public function set($key, $value)
+    {
+        if (isset($this->registry[$key])) {
+            throw new Exception("There is already an entry for key: ".$key);
+        }
+
+        $this->registry[$key] = $value;
+    }
+
+    /**
+     * Retrieves elements from registry array
+     * @param type $key
+     * @return type
+     * @throws Exception when no $key found
+     */
+    public function get($key)
+    {
+        if (!isset($this->registry[$key])) {
+            throw new Exception("There is no entry for key ".$key);
+        }
+
+        return $this->registry[$key];
+    }
+
+    public function offsetExists($key)
+    {
+        return isset($this->registry[$key]);
+    }
+
+    public function offsetGet($key)
+    {
+        if (isset($this->registry[$key])) {
+            return $this->registry[$key];
+        }
+
+        return null;
+    }
+
+    public function offsetSet($key, $value)
+    {
+        if (is_null($key)) {
+            $this->registry[] = $value;
+        } else {
+            $this->registry[$key] = $value;
+        }
+    }
+
+    public function offsetUnset($key)
+    {
+        unset($this->registry[$key]);
+    }
+
+}
