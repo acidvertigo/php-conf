9062d68bc6c898924dedf56c847fd2f069105c40
acidvertigo <ldanzelmo@gmail.com>
2015-02-03 22:54:51 +0100

    initial commit

---
+++ core/Configloader.php
@@ -0,0 +1,83 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2015 Acidvertigo.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace acd;
+
+/**
+ * Configloader - Configuration file loader class
+ * @author Acidvertigo
+ */
+class Configloader implements \ArrayAccess
+{
+
+    /** @var array $data * */
+    public $data = array();
+
+    /**
+     * Parses a INI file as an array
+     * @param  string $path
+     * @return array
+     * @throws ParseException If there is an error parsing the INI file
+     */
+    public function __construct($path)
+    {
+        $this->data = @parse_ini_file($path, true);
+        if (!$this->data) {
+            throw new \Exception("Configuration file not found: ".$path);
+        }
+
+        return $this->data;
+    }
+
+    public function offsetExists($key)
+    {
+        return isset($this->data[$key]);
+    }
+
+    public function offsetGet($key)
+    {
+        if (isset($this->data[$key])) {
+            return $this->data[$key];
+        }
+
+        return null;
+    }
+
+    public function offsetSet($key, $value)
+    {
+        if (is_null($key)) {
+            $this->data[] = $value;
+        } else {
+            $this->data[$key] = $value;
+        }
+    }
+
+    public function offsetUnset($key)
+    {
+        unset($this->data[$key]);
+    }
+
+}
