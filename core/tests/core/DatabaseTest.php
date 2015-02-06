a83854dd09f2f09675688ef2aa0067d4e12e0eb2
Luca <ldanzelmo@gmail.com>
2015-02-06 13:36:54 +0100

    Update Database Tests

--- core/tests/core/DatabaseTest.php
+++ core/tests/core/DatabaseTest.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -14,21 +14,80 @@
 require_once 'core/Database.php';
 require_once 'core/Registry.php';
 
-class DatabaseTest extends \PHPUnit_Framework_TestCase
-{    
-    public function testconnect()
+class DatabaseTest extends \PHPUnit_Extensions_Database_TestCase
+{
+
+    /**
+     * This is the object that will be tested
+     * @var DataPump
+     */
+    protected $object;
+
+    /**
+     * only instantiate pdo once for test clean-up/fixture load
+     * @var PDO
+     */
+    static private $pdo = null;
+
+    /**
+     * only instantiate PHPUnit_Extensions_Database_DB_IDatabaseConnection once per test
+     * @var type 
+     */
+    private $conn = null;
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
     {
-        $registry = \acd\Registry::getInstance();
-        
+        $registry = acd\Registry::getInstance();
         $registry->set('config', array('database' => array('HOST' => 'localhost', 
                                                            'NAME' => 'shop', 
                                                            'USERNAME' => 'root',
                                                            'PASSWORD' =>'' )));
-        
-        $firstCall = \acd\Database::connect($registry);
-        $this->assertInstanceOf('\PDO', $firstCall);
-        $secondCall = \acd\Database::connect($registry);
-        $this->assertSame($firstCall, $secondCall);
+        $this->object = acd\Database::connect($registry);
     }
 
- }    
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+        
+    }
+
+    protected function getConnection()
+    {
+        if ($this->conn === null) {
+            if (self::$pdo == null) {
+                self::$pdo = new PDO('mysql:dbname=shop;host=localhost', 'root', '');
+            }
+            $this->conn = $this->createDefaultDBConnection(self::$pdo, 'ross_testing');
+        }
+        return $this->conn;
+    }
+
+    protected function getDataSet()
+    {
+        return $this->createMySQLXMLDataSet(__DIR__ . '/datapump.xml');
+    }
+
+    /**
+     * This is here to ensure that the database is working correctly
+     */
+    public function testDataBaseConnection()
+    {
+
+        $this->getConnection()->createDataSet(array('products'));
+        $prod = $this->getDataSet();
+        $queryTable = $this->getConnection()->createQueryTable(
+                'products', 'SELECT * FROM products'
+        );
+        $expectedTable = $this->getDataSet()->getTable('products');
+        //Here we check that the table in the database matches the data in the XML file
+        $this->assertTablesEqual($expectedTable, $queryTable);
+    }
+
+}
