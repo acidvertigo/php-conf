eaec11466486005453ffb3fa760cc1fe18df96a6
Luca <ldanzelmo@gmail.com>
2015-02-10 14:25:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // Import core libraries.
-require 'core/ArrayAccess.php';
+require_once 'core/ArrayAccess.php';
 require_once 'core/Configloader.php';
 require_once 'core/Database.php';
 require_once 'core/Registry.php';
