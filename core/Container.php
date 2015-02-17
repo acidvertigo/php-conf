2d826a437c28962032de33b0eafe91d760cb7d08
Luca <ldanzelmo@gmail.com>
2015-02-17 17:58:29 +0100

    Add a DI Container

---
+++ core/Container.php
@@ -0,0 +1,96 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2015 Acidvertigo.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace acd;
+
+require_once 'core/ArrayAccess.php';
+
+/**
+ * Dependency injection container class
+ *
+ * @author acidvertigo
+ */
+class Container implements \ArrayAccess
+{
+
+    use ArrayAccess;
+        
+    /**
+    * Container Array
+    * @var array
+    */
+    protected $values = array();
+
+    /**
+    * Adds an item to the container
+    * @param string $key
+    * @param mixed $value
+    * @return void
+    */
+    public function add($key, $value)
+    {
+        if ($this->exists($key)) {
+            throw new \InvalidArgumentException("[{$key}] not found");
+        }
+        return $this->values[$key] = $value;
+    }
+
+    /**
+     * Returns item from container
+     * @param string $name
+     * @return mixed
+     */
+    public function make($name)
+    {
+        if (!$this->exists($name)) {
+            throw new \InvalidArgumentException("[{$name}] not found");
+        }
+        return $this->find($name);
+    }
+
+    /**
+     * Finds a Value
+     * @param  mixed $key
+     * @return mixed
+    */
+    private function find($key)
+    {
+        if (is_object($key)) {
+            return $this->values[$key]();
+        }
+        return $this->values[$key];
+    }
+
+    /**
+     * Checks if a key exists in container
+     * @param  string $key
+     * @return bool
+     */
+    private function exists($key)
+    {
+        return array_key_exists($key, $this->values);
+    }
+}
--- index.php
+++ index.php
@@ -3,7 +3,16 @@
 require_once 'core/Configloader.php';
 require_once 'core/Database.php';
 require_once 'core/Registry.php';
+require_once 'core/Container.php';
 
+$container = new \acd\Container;
+
+$container->add('registry', function()
+{
+    return \acd\Registry::getInstance();
+});
+$container->make('registry');
+var_dump($container);
 try {
 
     // Get an instance of the Registry
