28d5eea11b0562fb74b5ebd16c472136bac9630a
Luca <acidvertigo@users.noreply.github.com>
2015-10-20 22:14:21 +0200

    fix config array

--- include/config.php
+++ include/config.php
@@ -1,12 +1,12 @@
 <?php
 
 //This is a sample configuration file
-return array (database(
+return Array('database' => Array(
 'HOST' => 'localhost',
 'NAME' => 'shop',
 'USERNAME' => 'root',
 'PASSWORD' => '')
 ),
-(test(
+('test' => Array(
 'USERNAME' => 'test',
 'PASSWORD' => 'test2'));
