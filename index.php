083691f167f5bc6dac1fe40599e0572e7b7f9119
acidvertigo <ldanzelmo@gmail.com>
2015-10-25 11:37:39 +0100

    Make vendor autload optional

--- index.php
+++ index.php
@@ -1,22 +1,25 @@
 <?php
 // Import core libraries.
-require './vendor/autoload.php';
+if (file_exists('./vendor/autoload.php')) {
+  require './vendor/autoload.php';
+}
+
+// class autoload
 require_once 'Autoloader.php';
 
-    $registry = new Acd\Registry;
+$registry = new Acd\Registry;
 
-    $config = new Acd\Configloader('include/config.php');
+$config = new Acd\Configloader('include/config.php');
 
-    // Loads configuration into the registry
-	foreach($config->loadconfig() as $key => $value) {
-        $registry->set($key, $value);
-	}
+// Loads configuration into the registry
+foreach($config->loadconfig() as $key => $value) {
+    $registry->set($key, $value);
+}
 
-    $database = new Acd\Database($registry->get('database'));
+$database = new Acd\Database($registry->get('database'));
 
-    // Connect to database
-    $database = $database->connect();
-
+// Connect to database
+$database = $database->connect();
 
 foreach ($registry->get('database') as $key => $value) {
 	echo 'Key = ' . $key . ' Value = ' . $value . '<br>';
