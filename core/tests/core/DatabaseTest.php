2902974c1a54d5a00cef87575358909bb52dcacb
Luca <ldanzelmo@gmail.com>
2015-10-25 21:16:41 +0100

    Update DatabaseTest.php

--- core/tests/core/DatabaseTest.php
+++ core/tests/core/DatabaseTest.php
@@ -46,7 +46,7 @@ class DatabaseTest extends \PHPUnit_Extensions_Database_TestCase {
 
     $this->options = [\PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_OBJ, \PDO::ATTR_ERRMODE => \PDO::ERRMODE_WARNING];  
     $this->config = ['database' => [
-        'HOST' => 'localhost',
+        'HOST' => '127.0.0.1',
         'NAME' => 'shop',
         'USERNAME' => 'root',
         'PASSWORD' => '']];
