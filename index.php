85b1dfdd548c96e8880f063e9210a04423357548
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2015-10-25 22:03:06 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- index.php
+++ index.php
@@ -6,9 +6,9 @@ ini_set('display_errors', true);
 
 // Check for required PHP version   
 if (version_compare(PHP_VERSION, '5.6.0', '<'))
-  {
+    {
     exit(sprintf('This app requires PHP 5.6 or higher. Your PHP version is: %s.',PHP_VERSION));
-  }
+    }
 }
 
 // Import external libraries.
@@ -34,6 +34,6 @@ $database = new Acd\Database($registry->get('database'));
 $database = $database->connect();
 
 foreach ($registry->get('database') as $key => $value) {
-    echo 'Key = ' . $key . ' Value = ' . $value . '<br>';
+    echo 'Key = '.$key.' Value = '.$value.'<br>';
 }
 
