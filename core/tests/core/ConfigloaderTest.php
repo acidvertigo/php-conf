98b8b3e93f9e91ad17178357acfb18abf5e24e70
acidvertigo <ldanzelmo@gmail.com>
2015-10-25 12:07:26 +0100

    test file exists

--- core/tests/core/ConfigloaderTest.php
+++ core/tests/core/ConfigloaderTest.php
@@ -41,6 +41,7 @@ class ConfigloaderTest extends \PHPUnit_Framework_TestCase
     public function testLoadconfig()
     {
         $loadconfig = new \Acd\Configloader($this->path);
+		$this->assertFileExists($this->path);
         $this->assertAttributeInternalType('array', 'data', $loadconfig);
     }		
 }
