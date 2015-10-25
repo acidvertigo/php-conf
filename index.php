f0e28c4cca5280a3965bed9532597386e7440b12
Luca <acidvertigo@users.noreply.github.com>
2015-10-25 17:27:53 +0100

    fix comments

--- index.php
+++ index.php
@@ -4,17 +4,17 @@ error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', true);
 
 // Check for required PHP version
-if (version_compare(PHP_VERSION, '5.4', '<'))
+if (version_compare(PHP_VERSION, '5.4.0', '<'))
 {
     exit(sprintf('This app requires PHP 5.4 or higher. Your PHP version is: %s.',PHP_VERSION));
 }
 
-// Import core libraries.
+// Import external libraries.
 if (file_exists('./vendor/autoload.php')) {
     require './vendor/autoload.php';
 }
 
-// class autoload
+// Core class autoload
 require_once 'Autoloader.php';
 
 $registry = new Acd\Registry;
