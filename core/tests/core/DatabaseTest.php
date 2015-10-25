8f3817d1a461ae4b425711d413f972401d22620b
acidvertigo <ldanzelmo@gmail.com>
2015-10-25 11:26:25 +0100

    Fix databse test

--- core/tests/core/DatabaseTest.php
+++ core/tests/core/DatabaseTest.php
@@ -119,10 +119,10 @@ class DatabaseTest extends \PHPUnit_Extensions_Database_TestCase {
     public function testConnectionException() {
         $options = array(\PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_OBJ, \PDO::ATTR_ERRMODE => \PDO::ERRMODE_WARNING);
 		
-		$config = array('HOST' => 'lollocalhost',
-                        'NAME' => 'shop',
-                        'USERNAME' => 'root',
-                        'PASSWORD' => '',
+		$config = array('HOST' => 'localhost',
+                        'NAME' => 'shopshop',
+                        'USERNAME' => 'rootroot',
+                        'PASSWORD' => 'root',
                         $options);
 		
 		$database = new \Acd\Database($config);
