db19e25ae28a7c6cdf38e90fbe39117697d4ce92
Luca <ldanzelmo@gmail.com>
2015-10-26 17:38:50 +0100

    Create Request Class

---
+++ core/Request.php
@@ -0,0 +1,32 @@
+<?php 
+  
+namespace Acd; 
+  
+/** 
+* Request Class
+* @author Acidvertigo MIT Licence 
+*/
+
+class Request 
+{
+
+  private $headers = [];
+
+  /** 
+   * Check HTTP response headers
+   * @param string $url 
+   * @return array list of response headers 
+   */
+  public function getRequestHeaders($url = null)
+  {  
+
+    if (getallheaders() == FALSE) {
+      throw new \InvalidArgumentException('Unable to get Request Headers');
+    } else {
+      $this->headers[] = getallheaders();
+    }  
+
+      return $this->headers;
+  }
+
+}
