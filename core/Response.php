4ecd57ea52e24fcef387008f061e57d9187947f5
Luca <ldanzelmo@gmail.com>
2015-10-26 16:19:22 +0100

    Create Response Class

---
+++ core/Response.php
@@ -0,0 +1,46 @@
+<?php 
+  
+namespace Acd; 
+  
+/** 
+* Response Class
+* @author Acidvertigo MIT Licence 
+*/
+
+class Response 
+{ 
+  private $headers = [];
+
+  /** 
+   * Check HTTP response headers
+   * @param string $url 
+   * @return array list of response headers 
+   */
+  public function getResponseHeaders($url = null)
+  {  
+
+    if (get_headers($url, 1) == FALSE) {
+      throw new \InvalidArgumentException('Unable to get Response Headers');
+    } else {
+      $this->headers[] = get_headers($url, 1);
+    }  
+
+      return $this->headers;
+  }
+
+  /** 
+   * @param string $url 
+   * @return string HTTP response code 
+   */
+  public function getStatusCode($url = null)
+  {
+    $codeStatus = (int)substr($this->getResponseHeaders($url)[0], 9, 3);
+
+    if ($codeStatus < 100 || $codeStatus > 999) { 
+        throw new \InvalidArgumentException('Invalid  status code: ' . $statusCode); 
+    }
+    
+    return $codeStatus;
+  }
+
+}
