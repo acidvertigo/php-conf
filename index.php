e71f93c3109630d2519c2200fe7d26f183d8fdac
Luca <ldanzelmo@gmail.com>
2015-10-27 09:25:20 +0100

    Check PHP Version before everything

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php
 
-error_reporting(E_ALL | E_STRICT);
-ini_set('display_errors', true);
-
-
 // Check for required PHP version   
 if (version_compare(PHP_VERSION, '5.6.0', '<'))
 {
     exit(sprintf('This app requires PHP 5.6 or higher. Your PHP version is: %s.', PHP_VERSION));
 }
+
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', true);
 
 // Import external libraries.
 if (file_exists('./vendor/autoload.php')) {
