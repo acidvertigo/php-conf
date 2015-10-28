81b9d711028afc9809fef36c80c6b95d8eae2faf
acidvertigo <ldanzelmo@gmail.com>
2015-10-28 23:03:11 +0100

    Update comments

--- core/Response.php
+++ core/Response.php
@@ -15,7 +15,8 @@ class Response
     /** 
      * Check HTTP response headers
      * @param string|null $url 
-     * @return array list of response headers 
+     * @return array a list of response headers
+	 * @throws \InvalidArgumentException if response headers are null
      */
     public function getResponseHeaders($url = null)
     {
@@ -32,7 +33,8 @@ class Response
 
     /** 
      * @param string|null $url 
-     * @return integer HTTP response code 
+     * @return integer HTTP response code
+	 * @throws \InvalidArgumentException if the status code is outside range
      */
     public function getStatusCode($url = null)
     {
