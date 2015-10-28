3d082e10ce5fa1c0d50ecd251a7bf333879cfd8c
Luca <ldanzelmo@gmail.com>
2015-10-29 00:51:35 +0100

    Fix identation

--- core/Http.php
+++ core/Http.php
@@ -24,7 +24,7 @@ class Http
      */
     public function isSsl()
     {
-    return (!empty($_SERVER['HTTPS'] && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443)) ? TRUE : FALSE;
+        return (!empty($_SERVER['HTTPS'] && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443)) ? TRUE : FALSE;
     }
 
     /** 
