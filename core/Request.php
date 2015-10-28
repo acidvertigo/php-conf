143c97c98d9fa3104f74436c6c704a62c3164fef
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2015-10-28 20:12:54 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- core/Request.php
+++ core/Request.php
@@ -19,34 +19,34 @@ class Request
      */
     public function getRequestHeaders()
     {
-      if (function_exists('getallheaders()'))
-      {
-          $this->headers = getallheaders();
-      } else
-      {
-          $this->headers = $this->getServerHeaders();
-      }
-      if ($this->headers !== null)
-      {
-          return $this->headers;
-      } else
-      {
-          throw new \InvalidArgumentException('Unable to get Request Headers');
-      }
+        if (function_exists('getallheaders()'))
+        {
+            $this->headers = getallheaders();
+        } else
+        {
+            $this->headers = $this->getServerHeaders();
+        }
+        if ($this->headers !== null)
+        {
+            return $this->headers;
+        } else
+        {
+            throw new \InvalidArgumentException('Unable to get Request Headers');
+        }
     }
 
-   /** 
-    * Helper function if getallheaders() not available
-    * @return array list of response headers 
-    */
-  private function getServerHeaders() {
+    /** 
+     * Helper function if getallheaders() not available
+     * @return array list of response headers 
+     */
+    private function getServerHeaders() {
     foreach (array_keys($_SERVER) as $skey) {
-      if (strpos($skey, 'HTTP_') !== FALSE)
-      {
+        if (strpos($skey, 'HTTP_') !== FALSE)
+        {
         $this->headers[str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($skey, 5)))))] = $_SERVER[$skey];
-      }
+        }
     }
 
     return $this->headers;
-  }
+    }
 }
