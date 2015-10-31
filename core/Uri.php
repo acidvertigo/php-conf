191a4bddcf61cae066f07bb566768369f3ddf19d
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2015-10-31 13:46:54 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- core/Uri.php
+++ core/Uri.php
@@ -15,18 +15,18 @@ namespace Acd;
  */
 class Uri {
 	
-	private $http;
+    private $http;
 	
-	public function __construct(\Acd\Http $http) {
-		$this->http = $http;
-	}
+    public function __construct(\Acd\Http $http) {
+        $this->http = $http;
+    }
 	
-	/**
-	 * returns the current url
-	 * @return bool|string the current url
-	 */
-	public function getUrl() {
-		$url = ($this->http->isSsl() ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . htmlspecialchars( $_SERVER['REQUEST_URI'], ENT_QUOTES | ENT_HTML5, 'UTF-8' );
-		return filter_var($url, FILTER_SANITIZE_URL);
-	}
+    /**
+     * returns the current url
+     * @return bool|string the current url
+     */
+    public function getUrl() {
+        $url = ($this->http->isSsl() ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . htmlspecialchars( $_SERVER['REQUEST_URI'], ENT_QUOTES | ENT_HTML5, 'UTF-8' );
+        return filter_var($url, FILTER_SANITIZE_URL);
+    }
 }
