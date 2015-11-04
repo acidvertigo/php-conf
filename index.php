64938d39740688843d26f18c834d5541ef5cdf11
Luca <ldanzelmo@gmail.com>
2015-11-04 13:53:37 +0100

    Update Comments

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ if (version_compare(PHP_VERSION, '5.6.0', '<'))
     exit(sprintf('This app requires PHP 5.6 or higher. Your PHP version is: %s.', PHP_VERSION));
 }
 
+// Set Error Reporting
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', true);
 
@@ -18,9 +19,16 @@ if (file_exists('./vendor/autoload.php'))
 // Core class autoload
 require_once 'Autoloader.php';
 
+// Initialize main class
 $app = new Acd\Main();
+
+// Load configuration file
 $config = new Acd\Configloader('include/config.php');
+
+// Put configuration array into the Registry
 $registry = new Acd\Registry($config);
+
+//Start database connection
 $app->init('database', $registry);
 
 $headers = $app->getHeaders();
