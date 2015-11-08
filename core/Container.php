d1d2cbdca1c81098a325bd2b0b1d863283e87b60
acidvertigo <ldanzelmo@gmail.com>
2015-11-08 01:54:37 +0100

    Code Clean

--- core/Container.php
+++ core/Container.php
@@ -18,95 +18,96 @@ class Container
 
 	private $instances = [];
 
-	/**
-	 * Build an instance of the given class
-	 * @param string $class
-	 * @param array $args
-	 * @param bool $shared
-	 * @return \Acd\class
-	 * @throws \Exception
-	 */
-	public function resolve($class, array $args = [], $shared = TRUE)
-	{
-		//Is there a shared instance set? Return it.
-		if ($shared !== FALSE && isset($this->instances[$class]))
-		{
-			return $this->instances[$class];
-		}
+    /**
+     * Build an instance of the given class
+     * @param string $class
+     * @param array $args
+     * @param bool $shared
+     * @return \Acd\class
+     * @throws \Exception
+     */
+    public function resolve($class, array $args = [], $shared = TRUE)
+    {
+        //Is there a shared instance set? Return it.
+        if ($shared !== FALSE && isset($this->instances[$class]))
+        {
+            return $this->instances[$class];
+        }
 
-		$resolver = new \ReflectionClass($class);
+        $resolver = new \ReflectionClass($class);
 
- 		if (!$resolver->isInstantiable())
- 		{
- 			throw new \Exception("[$class] is not instantiable");
- 		}
- 		
- 		$constructor = $resolver->getConstructor();
- 		
- 		if (!$constructor)
- 		{
- 			return new $class;
- 		}
- 		
- 		$parameters = $constructor->getParameters();
+        if (!$resolver->isInstantiable())
+        {
+            throw new \Exception("[$class] is not instantiable");
+        }
+ 
+        $constructor = $resolver->getConstructor();
+
+        if (!$constructor)
+       {
+            return new $class;
+       }
+
+       $parameters = $constructor->getParameters();
 
         if (empty($parameters))
         {
           return new $class;
         }
-		
-		$dependencies = $this->getDependencies($parameters);
 
-		if (!empty($args))
-		{
-			foreach ($args as $key => $value) {
-			    $dependencies[$key] = $value;
-			}
-		}
+        $dependencies = $this->getDependencies($parameters);
 
- 		$this->instances[$class] = $resolver->newInstanceArgs($dependencies);
-		 
-		return $this->instances[$class];
-	}
-	
-	/**
-	 * Build up a list of dependencies for a given methods parameters
-	 * @param array $parameters
-	 * @return array
-	 */
-	public function getDependencies($parameters)
-	{
-		$dependencies = [];
-		
-		foreach ($parameters as $parameter)
-		{
-			$dependency = $parameter->getClass();
-			 
-			if (is_null($dependency))
-			{
-				$dependencies[] = $this->resolveNonClass($parameter);
-			}
-			else {
-				$dependencies[] = $this->resolve($dependency->name);
-			}
-		}
-		return $dependencies;
-	}
-	
-	/**
-	 * Determine what to do with a non-class value
-	 *
-	 * @param \ReflectionParameter $parameter
-	 * @return mixed
-	 *
-	 * @throws Exception
-	 */
-	public function resolveNonClass(\ReflectionParameter $parameter)
-	{
-		if ($parameter->isDefaultValueAvailable())
-		{
-			return $parameter->getDefaultValue();
-		}
-		
-   }      
+        if (!empty($args))
+        {
+            foreach ($args as $key => $value)
+            {
+                $dependencies[$key] = $value;
+            }
+        }
+
+        $this->instances[$class] = $resolver->newInstanceArgs($dependencies);
+
+        return $this->instances[$class];
+    }
+
+    /**
+     * Build up a list of dependencies for a given methods parameters
+     * @param array $parameters
+     * @return array
+     */
+    public function getDependencies($parameters)
+    {
+        $dependencies = [];
+
+        foreach ($parameters as $parameter)
+        {
+            $dependency = $parameter->getClass();
+
+            if (is_null($dependency))
+            {
+                $dependencies[] = $this->resolveNonClass($parameter);
+            }
+            else {
+                $dependencies[] = $this->resolve($dependency->name);
+            }
+        }
+        return $dependencies;
+    }
+
+    /**
+     * Determine what to do with a non-class value
+     *
+     * @param \ReflectionParameter $parameter
+     * @return mixed
+     *
+     * @throws Exception
+     */
+    public function resolveNonClass(\ReflectionParameter $parameter)
+    {
+        if ($parameter->isDefaultValueAvailable())
+        {
+            return $parameter->getDefaultValue();
+        }
+
+   }
 }
